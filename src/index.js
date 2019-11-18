c23b207b867acd101e8f404bb4631f39056bebb6
Daniyar <daniyar.abc@gmail.com>
2019-11-18 22:14:52 +0300

    fix: add cursor pointer and babel parser

--- src/index.js
+++ src/index.js
@@ -292,13 +292,13 @@ async function imageToCanvas(data) {
 }
 canvas.addEventListener('mouseover', () => {
   if (pencil) {
-    canvas.style.cursor = "url('/src/img/pencil.svg'), auto";
+    canvas.style.cursor = "url('./src/img/pencil.svg'), auto";
   }
   if (bucket) {
-    canvas.style.cursor = "url('/src/img/paint-bucket.svg'), auto";
+    canvas.style.cursor = "url('./src/img/paint-bucket.svg'), auto";
   }
   if (colorPicker) {
-    canvas.style.cursor = "url('/src/img/choose-color.svg'), auto";
+    canvas.style.cursor = "url('./src/img/choose-color.svg'), auto";
   }
 });
 window.addEventListener('DOMContentLoaded', () => {
