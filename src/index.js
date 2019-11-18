fe9e006de30a0403b32aa9bfcfdff9907c885883
Daniyar <daniyar.abc@gmail.com>
2019-11-19 00:34:23 +0300

    fix: add cursor pointer and babel parser 9

--- src/index.js
+++ src/index.js
@@ -420,7 +420,7 @@ async function setLogin() {
   const outputText = document.querySelector('.login-text');
   const response = await fetch('https://api.github.com/user', {
     headers: {
-      Authorization: `token ${e08102e5d8e0a19a590ad44b2857b601c336fc8f}`,
+      Authorization: 'token e08102e5d8e0a19a590ad44b2857b601c336fc8f',
     },
   });
   const data = await response.json();
